469a646d97c0cc7dc78d30d2e5403b9421f0df6d
Felipe Barbosa Brito de Andrade <felipebandradedev@gmail.com>
2023-10-17 09:16:46 -0300

    Utilizando a classe de controle

---
+++ admin/noticia-exclui.php
@@ -0,0 +1,6 @@
+<?php
+require_once "../vendor/autoload.php";
+use Microblog\ControleDeAcesso;
+
+$sessao = new ControleDeAcesso;
+$sessao->verificaAcesso();
--- admin/usuario-exclui.php
+++ admin/usuario-exclui.php
@@ -1,6 +1,10 @@
 <?php
 require_once "../vendor/autoload.php";
 use Microblog\Usuario;
+use Microblog\ControleDeAcesso;
+
+$sessao = new ControleDeAcesso;
+$sessao->verificaAcesso();
 
 $usuario = new Usuario;
 $usuario->setId($_GET['id']);
