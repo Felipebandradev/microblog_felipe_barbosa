b78dbdbf852f6942222182b9d38490900d6c6c87
Felipe Barbosa Brito de Andrade <felipebandradedev@gmail.com>
2023-10-24 09:25:21 -0300

    operador "nullabel typehint"

--- src/Banco.php
+++ src/Banco.php
@@ -1,27 +1,38 @@
 <?php
+
 namespace Microblog;
+
 use PDO, Exception;
 
-abstract class Banco {
+abstract class Banco
+{
     private static string $servidor = "localhost";
     private static string $usuario = "root";
     private static string $senha = "";
     private static string $banco = "microblog_felipe_barbosa";
-    private static PDO $conexao; 
+    /* operador "nullabel typehint"
+        Quando usado indica que a propriedade/atributo
+         pode conter null ou pode ser tipo PDO
+        
+        Neste caso, a propriedade conexao é inicialiada como
+        nula, mas apartir do momento em que uma conexao é feita,
+         ela passa a valer PDO */
+    private static ?PDO $conexao = null;
 
-    public static function conecta():PDO {
-        if( self::$conexao === null ){
+    public static function conecta(): PDO
+    {
+        if (self::$conexao === null) {
             try {
                 self::$conexao = new PDO(
-                    "mysql:host=".self::$servidor."; 
-                    dbname=".self::$banco.";
+                    "mysql:host=" . self::$servidor . "; 
+                    dbname=" . self::$banco . ";
                     charset=utf8",
-                    self::$usuario, 
+                    self::$usuario,
                     self::$senha
                 );
-                self::$conexao->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                self::$conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             } catch (Exception $erro) {
-                die("Deu ruim: ".$erro->getMessage());
+                die("Deu ruim: " . $erro->getMessage());
             }
         }
         return self::$conexao;
