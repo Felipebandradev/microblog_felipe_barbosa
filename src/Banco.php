3dc16a7904146bca126efc1233f2f7f6557010cd
Felipe Barbosa Brito de Andrade <felipebandradedev@gmail.com>
2023-10-24 09:11:44 -0300

    Classe noticia

--- src/Banco.php
+++ src/Banco.php
@@ -10,17 +10,19 @@ abstract class Banco {
     private static PDO $conexao; 
 
     public static function conecta():PDO {
-        try {
-            self::$conexao = new PDO(
-                "mysql:host=".self::$servidor."; 
-                dbname=".self::$banco.";
-                charset=utf8",
-                self::$usuario, 
-                self::$senha
-            );
-            self::$conexao->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch (Exception $erro) {
-            die("Deu ruim: ".$erro->getMessage());
+        if( self::$conexao === null ){
+            try {
+                self::$conexao = new PDO(
+                    "mysql:host=".self::$servidor."; 
+                    dbname=".self::$banco.";
+                    charset=utf8",
+                    self::$usuario, 
+                    self::$senha
+                );
+                self::$conexao->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (Exception $erro) {
+                die("Deu ruim: ".$erro->getMessage());
+            }
         }
         return self::$conexao;
     }
